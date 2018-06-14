b850ddcb52ccfd4d2ac14e83cd62c58a4d9c4266
jiyifa <jiyifa@163.com>
2018-06-14 21:10:08 +0800

    erP

---
+++ src/main/java/com/example/mapper/EmployeeMapper.java
@@ -0,0 +1,14 @@
+package com.example.mapper;
+
+
+import com.example.pojo.TestName;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public  interface EmployeeMapper {
+   /* @Select("select * from name")*/
+    List<TestName> selectAll();
+
+}
